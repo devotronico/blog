c5bba2e2c0ff4a3d510155858ad8d5acee869c76
Daniele Manzi <dmanzi83@gmail.com>
2018-11-05 13:04:35 +0100

    28° commit

--- app/views/mobile/view-home/cover.tpl.php
+++ app/views/mobile/view-home/cover.tpl.php
@@ -1,11 +1,8 @@
 <!-- COVER -->
 <div id="cover">
-    <!-- <img id="cover-animation" src='https://media.giphy.com/media/duVyjvb0c43gQ/giphy.gif' width="100%" height="100%" style="position:absolute"> -->
-    <!-- <img id="cover-animation" src='img/gif/d_anim.gif' width="100%" height="100%" style="position:absolute"> -->
-    <!-- <img id="cover-animation" src='img/gif/d_anim.gif'> -->
     <div id="cover-container">
         <div id="cover-box-image">
-            <img id='cover-foto' src='/img/<?=$photo?>.jpg'> 
+            <img id='cover-foto' src='/img/photo/<?=$photo?>.jpg'> 
         </div>
         <div id="cover-box-info">
             <p id="cover-text">web developer</p>
--- layout/index.tpl.php
+++ layout/index.tpl.php
@@ -28,10 +28,10 @@
 <link rel="apple-touch-icon" sizes="144x144" href="/public/img/favicon/apple-icon-144x144.png">
 <link rel="apple-touch-icon" sizes="152x152" href="/public/img/favicon/apple-icon-152x152.png">
 <link rel="apple-touch-icon" sizes="180x180" href="/public/img/favicon/apple-icon-180x180.png">
-<link rel="icon" type="image/png" sizes="192x192"  href="/android-icon-192x192.png">
+<link rel="icon" type="image/png" sizes="192x192"  href="/public/img/favicon/android-icon-192x192.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/public/img/favicon/favicon-16x16.png">
 <link rel="icon" type="image/png" sizes="32x32" href="/public/img/favicon/favicon-32x32.png">
 <link rel="icon" type="image/png" sizes="96x96" href="/public/img/favicon/favicon-96x96.png">
-<link rel="icon" type="image/png" sizes="16x16" href="/public/img/favicon/favicon-16x16.png">
 <!-- <link rel="manifest" href="/public/img/favicon/manifest.json"> -->
 <meta name="msapplication-TileColor" content="#ffffff">
 <meta name="msapplication-TileImage" content="/public/img/favicon/ms-icon-144x144.png">
